2c813c1bc98ead1a04b22bc202ea27abf13815f5
Hassaan Zuberi <mhazuberi@gmail.com>
2021-05-28 00:33:18 -0600

    integrated Header and Footer components

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../src/ui/Theme';
+import React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../src/ui/Theme";
+import Header from "../src/ui/Header";
+import Footer from "../src/ui/Footer";
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
+  const [value, setValue] = React.useState(0);
+  const [selected, setSelected] = React.useState(null);
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
@@ -20,12 +24,26 @@ export default function MyApp(props) {
     <React.Fragment>
       <Head>
         <title>My page</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <Component {...pageProps} />
+        <Header
+          value={value}
+          setValue={setValue}
+          selected={selected}
+          setSelected={setSelected}
+        />
+        <Component
+          {...pageProps}
+          setValue={setValue}
+          setSelected={setSelected}
+        />
+        <Footer setValue={setValue} setSelected={setSelected} />
       </ThemeProvider>
     </React.Fragment>
   );
